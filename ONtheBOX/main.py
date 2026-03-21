d567f9471084bdfd76451c7019996c34cc9a9c26
RachitLochan <149613941+RachitLochan@users.noreply.github.com>
2026-03-21 13:21:27 +0530

    Refactor main.py to include Background and Stuff classes
    Updated game dimensions and added new classes for background and objects.

--- ONtheBOX/main.py
+++ ONtheBOX/main.py
@@ -1,9 +1,12 @@
 import pygame
 from sys import exit #exit() funtion to close game(properly #1)
+from player import Player
+from backgd import Background
+from stuff import Stuff
 clock=pygame.time.Clock() #clockobject made
 
-HEIGHT=600 #we need to write a code to automatically find the size of
-LENGTH=1000 #the screen of the player and adjust the screen size automatically i gess
+HEIGHT=100 #we need to write a code to automatically find the size of
+LENGTH=200 #the screen of the player and adjust the screen size automatically i gess
 FPS=12
 """we made them constant for easy to use"""
 
@@ -13,23 +16,29 @@ screen = pygame.display.set_mode((LENGTH,HEIGHT))
 pygame.display.set_caption("we are on the box")
 
 
-from background import Background
-Background(screen,(69,69,69),None)
 
-from player import Player
-tom=Player(screen,150,150,64,128,None,(100,200,150))
-jerry=Player(screen,150,0,64,64,None,(100,200,200))
-ground=Player(screen,0,536,1000,64,None,(100,255,200))
-
+world=Background(screen,(69,69,69),"background/SKY.png")
+#world=Background(screen,(69,69,69),None)
 """ object=pygame.Rect(300,400,32,32)
     pygame.draw.rect(screen,(0,250,250),object,0,1,100,-50,90,1110)
     test code to undersatnt Rect and rect
 """
 gameloop=True
+object=pygame.Rect(300,400,32,32)
+
+#define player and objects(for now object and plaer both by player class)
+tom=Player(screen,150,150,64,128,None,(100,200,150))
+jerry=Stuff(screen,150,0,64,64,None,(100,200,200))
+ground=Stuff(screen,0,536,1000,64,None,(100,255,200))
 
 while gameloop==True:
         
-
+    
+    
+    world.draw() 
+    jerry.draw() 
+    ground.draw()
+    tom.draw()
 
     for event in pygame.event.get():#pygame.event.get gives  all events happned in a [list] every frame (60 frame per sec) 
         if event.type == pygame.QUIT : #pygame.QUIT gives  1 if cross red is pressed 
@@ -39,6 +48,10 @@ while gameloop==True:
         if  event.type == pygame.KEYDOWN and event.key==pygame.K_ESCAPE:
             pygame.quit()
             exit()
-
+    tom.move()
+    
+    
+        
+    pygame.draw.rect(screen,(0,250,250),object,0,1,100,-50,90,1110)
     pygame.display.update() #load new screen
     clock.tick(FPS)
