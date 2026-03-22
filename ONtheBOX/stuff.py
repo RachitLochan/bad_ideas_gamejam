004e06adea72a3e9360e75e01ce92a7e2e67b649
RachitLochan <149613941+RachitLochan@users.noreply.github.com>
2026-03-22 11:02:07 +0530

    Update stuff.py
    need to fix color carcater one

--- ONtheBOX/stuff.py
+++ ONtheBOX/stuff.py
@@ -1,13 +1,22 @@
 import pygame
 
-class Stuff:
-    def __init__(self,gamewindow,startx,starty,fat,tall,image,colour):
-        self.caracter=pygame.Rect(startx,starty,fat,tall)  
+class Stuff(pygame.Rect):
+    def __init__(self,gamewindow,startx,starty,fat,tall,image=None,RATIO=None,colour=None):
+        pygame.Rect.__init__(self,startx,starty,fat,tall)
         self.gamewindow=gamewindow
-        self.colour=colour
-            
-    def draw(self):
-        pygame.draw.rect(self.gamewindow,self.colour,self.caracter)
+        if image!=None:
+            playerimage=pygame.image.load(image)
+            playerimage=pygame.transform.scale(playerimage,(fat,tall))
+            self.image=playerimage
 
+        if RATIO !=None:
+            self.image=pygame.transform.scale_by(self.image,RATIO) 
+
+    
+    def draw(self):
+        if self.image==None:
+            pygame.draw.rect(self.gamewindow,self.colour,self.caracter)
+        else:
+            self.gamewindow.blit(self.image,(self.topleft))
 
     
