8353098fc46de10c15fee31c377912f49bab8b5a
RachitLochan <149613941+RachitLochan@users.noreply.github.com>
2026-04-01 02:31:41 +0530

    Add Level4 to gameplay sequence

--- ONtheBOX/gameplay.py
+++ ONtheBOX/gameplay.py
@@ -1,7 +1,7 @@
 from levels import Level1
 from gg import Level2
 from level3 import Level3
-
+from level4 import Level4
 
 
 khela=Level1
@@ -12,6 +12,9 @@ khela.play(khela)
 
 
 khela=Level3
+khela.play(khela)
+
+khela=Level4
 khela.play(khela)
 
 
